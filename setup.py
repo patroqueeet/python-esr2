694344ac35c11aa275b16081044fe5a42ed2ae8f
Jirka Schaefer <jirka@tschitschereengreen.com>
2020-06-08 15:35:17 +0200

    make pip installable

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from distutils.sysconfig import get_python_lib
+
+from setuptools import find_packages, setup
+
+setup(
+    name='ESR Reader',
+    version=version,
+    url='https://github.com/patroqueeet/python-esr2',
+    author='patroqueeet',
+    author_email='patroqueeet@gmail.com',
+    description=('Read swiss BESR / v11 files'),
+    license='GPL',
+    packages='',
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
