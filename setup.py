b4307523d7108f2bbf98ff60fc3571b5c5496a6a
Jirka Schaefer <jirka@tschitschereengreen.com>
2020-06-08 22:52:48 +0200

    v0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='esr2',
-    version=0.5,
+    version=0.7,
     url='https://github.com/patroqueeet/python-esr2',
     author='patroqueeet',
     author_email='patroqueeet@gmail.com',
